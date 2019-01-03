5bcd8d07009787bc4f792bd1fb35e9b5f4121998
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-03 08:26:28 +0700

    Create rolecolor.js

---
+++ cmds/rolecolor.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+   if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("you don't have permssion MANAGE_ROLES to use this !").then(msg => msg.delete(6000));
+      let role = message.mentions.roles.first() || message.guild.roles.find('name', args[0]);
+   if(!role) return message.channel.send("You forgot to type or mention a role!");
+      let color = args.slice(1).join(" ");
+   if(!color) return message.channel.send("You forgot to type a color hex!");
+      await role.setColor(color).catch(error => message.channel.send(`Error: ${error}`));
+      await message.channel.send(`\`${role.name}\`'s color was changed to ${role.color}`).catch(error => message.channel.send(`Error: ${error}`));
+};
+
+exports.help = {
+    name: "rolecolor"
+}
