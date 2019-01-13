23192bba4a46da8a0139516e635a5b3a9cec0515
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-13 18:50:34 +0700

    Update youtube.js

--- cmds/youtube.js
+++ cmds/youtube.js
@@ -1,1 +1,22 @@
-hhjjkkbjjn
+const Discord = require("discord.js");
+const YouTube = require("simple-youtube-api");
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyDbEcpxDcLwU4M6krBHQloNeVc7M98zbC8");
+const embed = require("embed-video")
+
+exports.run = async (bot, message, args) => {
+    youtube.searchVideos(args, 1)
+      .then(results => {
+
+       const ytEmbed = new Discord.RichEmbed()
+        .setTitle(`Hasil Pencarian YouTube Untuk ${args}`.split(',').join(' '))
+        .setDescription(`https://www.youtube.com/watch?v=${results[0].id}`)
+
+        message.channel.send(ytEmbed);
+      })
+    .catch(console.log);
+}
+
+exports.help = {
+	name: "youtube"
+}
