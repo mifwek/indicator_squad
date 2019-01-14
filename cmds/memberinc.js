3c407b4a20e52158006c164154b4dbd8b8d7da91
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-14 23:57:19 +0700

    Update memberinc.js

--- cmds/memberinc.js
+++ cmds/memberinc.js
@@ -8,8 +8,7 @@ let patEmb = new Discord.RichEmbed()
    .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264424698216459/20190114_134618.jpg",
 "https://cdn.discordapp.com/attachments/514635773895507979/534264424698216460/20190114_135145.jpg",
 "https://cdn.discordapp.com/attachments/514635773895507979/534264425595666442/20190114_133509.jpg",
-"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666443/20190114_133621.jpg",
-);
+"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666443/20190114_133621.jpg");
 
 message.channel.send(patEmb);
 
