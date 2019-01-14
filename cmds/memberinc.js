550e8794ac9cf271b04bf3323fa6950611402959
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-14 14:01:57 +0700

    Update memberinc.js

--- cmds/memberinc.js
+++ cmds/memberinc.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js')
 
 exports.run = async (bot, message, args) => {
-    var images = ["https://cdn.discordapp.com/attachments/514635773895507979/534152302098120734/20190114_061710.jpg"];
+    var images = ["https://cdn.discordapp.com/attachments/514635773895507979/534264424698216459/20190114_134618.jpg",
+"https://cdn.discordapp.com/attachments/514635773895507979/534264424698216460/20190114_135145.jpg",
+"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666442/20190114_133509.jpg",
+"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666443/20190114_133621.jpg"
+];
     var rand = Math.floor(Math.random() * images.length);
     var randomImage = images[rand];
 
