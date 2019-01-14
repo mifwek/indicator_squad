063b321dcdecc29cab4bb9409e9d5ed3df90229e
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-15 00:23:25 +0700

    Update memberinc.js

--- cmds/memberinc.js
+++ cmds/memberinc.js
@@ -3,9 +3,8 @@ const Discord = require('discord.js')
 exports.run = async (bot, message, args) => {
 
 let patEmb = new Discord.RichEmbed()
-   .setTitle("ID Member Diambil Secara Random")
-   .setColor(0xA901DB)
-   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264424698216459/20190114_134618.jpg", "https://cdn.discordapp.com/attachments/514635773895507979/534264424698216460/20190114_135145.jpg", "https://cdn.discordapp.com/attachments/514635773895507979/534264425595666442/20190114_133509.jpg", "https://cdn.discordapp.com/attachments/514635773895507979/534264425595666443/20190114_133621.jpg");
+   .setTitle("**List ID Member Indicator Squad**")
+   .addField("Tersedia", "`mip` \n `hadi` \n `potato` \n `bima` \n `dinan` \n `pororo` \n `Snap` \n `dina`");
 
 message.channel.send(patEmb);
 
