5edbfc663a9529cdebcf5d08411570bf088f2de3
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-14 23:51:29 +0700

    Update memberinc.js

--- cmds/memberinc.js
+++ cmds/memberinc.js
@@ -1,20 +1,15 @@
 const Discord = require('discord.js')
 
 exports.run = async (bot, message, args) => {
-    var images = ["https://cdn.discordapp.com/attachments/514635773895507979/534264424698216459/20190114_134618.jpg",
-"https://cdn.discordapp.com/attachments/514635773895507979/534264424698216460/20190114_135145.jpg",
-"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666442/20190114_133509.jpg",
-"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666443/20190114_133621.jpg",
-"https://cdn.discordapp.com/attachments/514635773895507979/534280233394765824/20190114_145426.jpg",
-"https://cdn.discordapp.com/attachments/514635773895507979/534280233394765825/20190114_145814.jpg"
-];
-
-
 
 const patEmb = new Discord.RichEmbed()
    .setTitle("ID Member Diambil Secara Random")
    .setColor(0xA901DB)
-   .setImage(images);
+   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264424698216459/20190114_134618.jpg",
+"https://cdn.discordapp.com/attachments/514635773895507979/534264424698216460/20190114_135145.jpg",
+"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666442/20190114_133509.jpg",
+"https://cdn.discordapp.com/attachments/514635773895507979/534264425595666443/20190114_133621.jpg",
+);
 
 message.channel.send(patEmb);
 
