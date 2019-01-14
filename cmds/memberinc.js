0774499fa0b06b2efaca04b446fa77e3ce0e070e
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-14 23:54:15 +0700

    Update memberinc.js

--- cmds/memberinc.js
+++ cmds/memberinc.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 exports.run = async (bot, message, args) => {
 
-const patEmb = new Discord.RichEmbed()
+let patEmb = new Discord.RichEmbed()
    .setTitle("ID Member Diambil Secara Random")
    .setColor(0xA901DB)
    .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264424698216459/20190114_134618.jpg",
