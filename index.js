2723cc397f350daaac667f4d038de38ed3ace155
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-16 18:37:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} sudah online!`);
-	bot.user.setActivity("INDICATOR SQUAD", {type: "WATCHING"});
+	bot.user.setActivity(.setTimestamp("WIB")), {type: "LISTENING"});
 });
 
 bot.on('guildMemberAdd', async (member) => {
