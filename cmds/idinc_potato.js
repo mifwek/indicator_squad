0ec15d9c7d8767b9f606ac0b14c0362ca073af98
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-18 01:46:36 +0700

    Update idinc_potato.js

--- cmds/idinc_potato.js
+++ cmds/idinc_potato.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
 let patEmb = new Discord.RichEmbed()
    .setTitle("ID telah ditemukan")
    .setColor(0xA901DB)
-   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264425595666442/20190114_133509.jpg");
+   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/535526610271600660/20190118_005939.jpg");
 
 message.channel.send(patEmb);
 
