bac0c78f64a86cfd60800397fb866ba10842613c
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-18 01:45:45 +0700

    Update idinc_bima.js

--- cmds/idinc_bima.js
+++ cmds/idinc_bima.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
 let patEmb = new Discord.RichEmbed()
    .setTitle("ID telah ditemukan")
    .setColor(0xA901DB)
-   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264425595666443/20190114_133621.jpg");
+   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/535526587068710922/20190118_010104.jpg");
 
 message.channel.send(patEmb);
 
