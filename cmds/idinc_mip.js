a4668f56ba6341d174e9eeeb665fd1a06f4f3487
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-18 01:47:33 +0700

    Update idinc_mip.js

--- cmds/idinc_mip.js
+++ cmds/idinc_mip.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
 let patEmb = new Discord.RichEmbed()
    .setTitle("ID telah ditemukan")
    .setColor(0xA901DB)
-   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264424698216460/20190114_135145.jpg");
+   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/535526643108937728/20190117_215025.jpg");
 
 message.channel.send(patEmb);
 
