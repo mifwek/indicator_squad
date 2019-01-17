bd35122b93973174ef9123d51511f075dbb1a730
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-18 01:38:14 +0700

    Update idinc_suryo.js

--- cmds/idinc_suryo.js
+++ cmds/idinc_suryo.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
 let patEmb = new Discord.RichEmbed()
    .setTitle("ID telah ditemukan")
    .setColor(0xA901DB)
-   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534500311285366805/20190115_053253.jpg");
+   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/535526103784226834/20190118_011921.jpg");
 
 message.channel.send(patEmb);
 
