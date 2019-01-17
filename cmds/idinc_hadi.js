4266824721627bab6fe71ec461687db7a1f9d692
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-18 01:44:46 +0700

    Update idinc_hadi.js

--- cmds/idinc_hadi.js
+++ cmds/idinc_hadi.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
 let patEmb = new Discord.RichEmbed()
    .setTitle("ID telah ditemukan")
    .setColor(0xA901DB)
-   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534264424698216459/20190114_134618.jpg");
+   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/535526544874143765/20190118_010409.jpg");
 
 message.channel.send(patEmb);
 
