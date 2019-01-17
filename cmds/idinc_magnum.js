f07244581228e7968bbf213271235b704b860508
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-18 01:34:59 +0700

    Update idinc_magnum.js

--- cmds/idinc_magnum.js
+++ cmds/idinc_magnum.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
 let patEmb = new Discord.RichEmbed()
    .setTitle("ID telah ditemukan")
    .setColor(0xA901DB)
-   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/534656493266403338/20190115_155338.jpg");
+   .setImage("https://cdn.discordapp.com/attachments/514635773895507979/535526003628310548/20190118_012122.jpg");
 
 message.channel.send(patEmb);
 
