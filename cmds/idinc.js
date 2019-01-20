3562a216448d82b774d800d9acff1920aba2678b
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-20 15:29:55 +0700

    Update idinc.js

--- cmds/idinc.js
+++ cmds/idinc.js
@@ -4,7 +4,7 @@ exports.run = async (bot, message, args) => {
 
 var embedhelpadmin = new Discord.RichEmbed()
             .setTitle("List nama member indicator squad")
-            .addField("Tersedia Sementara", "`mip`, `hadi`, `potato`, `bima`, `dinan`, `pororo`, `Snap`, `dina`, `suryo`, `papah`, \n`magnum`, `taufik`, `rifki`")
+            .addField("Tersedia Sementara", "`mip`, `hadi`, `potato`, `bima`, `dinan`, `pororo`, `Snap`, `dina`, `suryo`, `papah`, \n`magnum`, `taufik`, `rifki`, `mail`")
             .addField("Contoh :", "`m=idinc_<nama member>`")
             .setColor(0x00FFEE)
             .setTimestamp();
