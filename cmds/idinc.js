e4c8c6842841a0d29096d0d92a44b2f6fddc059b
mifwek <44892124+mifwek@users.noreply.github.com>
2019-01-23 15:52:30 +0700

    Update idinc.js

--- cmds/idinc.js
+++ cmds/idinc.js
@@ -7,7 +7,7 @@ var embedhelpadmin = new Discord.RichEmbed()
             .addField("Tersedia Sementara", "`mip`, `hadi`, `potato`, `bima`, `dinan`, `pororo`, `Snap`, `dina`, `suryo`, `papah`, \n`magnum`, `taufik`, `rifki`, `mail`")
             .addField("Contoh :", "Untuk menampilkan gambar ID member `INC`, \nKetikkan perintah berikut ; \n`m=idinc_<nama member>`")
             .setColor(0x00FFEE)
-            .setTimestamp();
+            .setFooter("By ♤IŋᎠῖҪαϯϴR♤ BOT")
 
             message.channel.send(embedhelpadmin)
     };
