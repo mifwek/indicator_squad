f53cf0a51711c16f4f2198e97f81cd7bbf9d23a7
mifwek <44892124+mifwek@users.noreply.github.com>
2019-02-05 16:47:05 +0700

    Update report.js

--- cmds/report.js
+++ cmds/report.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
 	if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Anda tidak punya akses ini");
-    var channel = message.guild.channels.find('name', '🔸report💢');
+    var channel = message.guild.channels.find('name', 'report');
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if (!member)
         return message.reply("Pengguna tidak ditemukan.");
@@ -10,7 +10,7 @@ exports.run = async (bot, message, args) => {
     if (!reason) reason = "Tidak diinformasikan.";
     let embed = new Discord.RichEmbed()
         .setThumbnail(message.author.avatarURL)
-        .setTitle(`report`)
+        .setTitle(`REPORT`)
         .addField('Yang melaporkan:', message.author)
         .addField('Yang dilaporkan:', member.user)
         .addField('Alasan:', reason)
