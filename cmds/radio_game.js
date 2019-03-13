43aea3ac5c2f4cc2e91a378e8e6d8a5f2f3fc843
mifwek <44892124+mifwek@users.noreply.github.com>
2019-03-13 18:13:16 +0700

    Update radio_game.js

--- cmds/radio_game.js
+++ cmds/radio_game.js
@@ -15,16 +15,6 @@ exports.run = async (bot, message, args) => {
     
     }
     
-    if(!message.member.voiceChannel) {
-        message.channel.sned("Saya pikir ini dapat bekerja lebih baik jika Anda berada di channel voice!");
-    }
-
-    if(!servers[message.guild.id]) servers[message.guild.id] = {
-        queue: []
-    }
-    var server = servers[message.guild.id];
-
-    server.queue.push(args[0]);
     message.channel.send("✔️ | Selamat Mendengarkan.")
     if(!message.member.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
         play(connection, message);
