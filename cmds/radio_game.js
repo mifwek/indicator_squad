086cb6eebb4e0fa7e62be8d7f0c1e9c61504d1da
mifwek <44892124+mifwek@users.noreply.github.com>
2019-03-13 17:51:44 +0700

    Create radio_game.js

---
+++ cmds/radio_game.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const YTDL = require("https://www.rmfon.pl/play,156");
+function play(connection, message) {
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function() {
+        if(server.queue[0]) play(connection, messsage);
+        else connection.disconnect();
+    })
+}
+var servers = {};
+exports.run = async (bot, message, args) => {
+    //play
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Gua males muterin lagu buat lu 😅");
+    if (!args[0]) {
+         message.channel.send("Silakan tentukan link");
+         return
+    }
+    
+    if(!message.member.voiceChannel) {
+        message.channel.sned("Saya pikir ini dapat bekerja lebih baik jika Anda berada di channel voice!");
+    }
+
+    if(!servers[message.guild.id]) servers[message.guild.id] = {
+        queue: []
+    }
+    var server = servers[message.guild.id];
+
+    server.queue.push(args[0]);
+    message.channel.send("✔️ | Selamat Mendengarkan.")
+    if(!message.member.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+        play(connection, message);
+    })
+}
+
+exports.help = {
+    name: "play"
+}
