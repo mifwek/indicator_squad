df7897b983ed78e41e5248151930b165000e8c73
mifwek <44892124+mifwek@users.noreply.github.com>
2019-03-13 18:00:51 +0700

    Update radio_game.js

--- cmds/radio_game.js
+++ cmds/radio_game.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
-const YTDL = require("https://www.rmfon.pl/play,156");
+const YTDL = require("");
 function play(connection, message) {
     var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.dispatcher = connection.playStream("https://www.rmfon.pl/play,156"(server.queue[0], {filter: "audioonly"}));
     server.queue.shift();
     server.dispatcher.on("end", function() {
         if(server.queue[0]) play(connection, messsage);
