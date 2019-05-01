9a328cf8ca95dd966048b9f912c13b93468f3855
mifwek <44892124+mifwek@users.noreply.github.com>
2019-05-01 16:54:46 +0700

    Delete radio.js

--- cmds/radio.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-exports.run = async (bot, message, args) => {
-   message.member.voiceChannel.join().then(x => x.playStream('https://listen.moe/opus'));
-}
-
-exports.help = {
-	name: "radio"
-}
